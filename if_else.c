db9aacea5f6e2cb564f65b141765c89194345201
Victor Kanu <victorchimakanu@gmail.com>
2023-06-21 18:54:31 +0100

    if_else.c

---
+++ if_else.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+
+int a = 50;
+
+if (a > 50)
+{ 
+	printf("my goodness you're getting old\n");
+}
+else 
+{
+	printf("a is not greater than 50\n");
+}
+
+printf("value of a is : %d\n" , a);
+
+return (0);
+
+}
